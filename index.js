3ae232aa879ff636f39c7f900ce8f6f184958423
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-03-30 21:02:20 -0400

    index: fix port binding

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`server start on port ${PORT}`);
 })
