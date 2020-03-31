d8524f8d922d8a05fdb61a24bc9cba65c534c48a
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-03-30 20:43:20 -0400

    init

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
     res.send({ hi: 'there' });
 });
 
-app.listen(3000, () => {
+
+const PORT = process.env.PORT | 5000;
+app.listen(PORT, () => {
     console.log('server start on port 3000');
 })
