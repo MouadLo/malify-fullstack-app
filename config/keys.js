8285d6010df5e74311354c88dc5cce9ad385297a
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-04-01 04:51:24 -0400

    Determining Environment

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+if (process.env.NODE_ENV === 'production') {
+	// we are in production - return the prod set of keys
+	module.exports = require('./prod');
+} else {
+	// We are in development - return the dev  keys !!!!
+	module.exports = require('./dev');
+}
+52d84ulp1k16cafq4p5
+917905622684 - sg37gmosl0ds3bq7m0b8kv9r4lq38t20.apps.googleusercontent.com
+5Or2f7P7xqWvrCQXfGRQlT-s
---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoUri: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY
+};
+52d84ulp1k16cafq4p5
+917905622684 - sg37gmosl0ds3bq7m0b8kv9r4lq38t20.apps.googleusercontent.com
+5Or2f7P7xqWvrCQXfGRQlT-s
