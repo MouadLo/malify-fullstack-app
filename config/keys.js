865961739f560053e452cee7d27faeaf32a49973
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-04-01 04:52:02 -0400

    Determining Environment

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,7 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
 	// we are in production - return the prod set of keys
-	module.exports = require('./prod');
+	module.exports = require("./prod");
 } else {
 	// We are in development - return the dev  keys !!!!
-	module.exports = require('./dev');
+	module.exports = require("./dev");
 }
-52d84ulp1k16cafq4p5
-917905622684 - sg37gmosl0ds3bq7m0b8kv9r4lq38t20.apps.googleusercontent.com
-5Or2f7P7xqWvrCQXfGRQlT-s
