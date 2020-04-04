90224ccee6df0ee3fcacf38fd20913eee2cc652d
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-04-04 17:46:35 -0400

    client: redux-thunk setup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,18 @@ import React, { Component } from 'react';
 
 class Header extends Component {
 	render() {
-		return <div>Header</div>;
+		return (
+			<nav>
+				<div className="nav-wrapper light-blue darken-4">
+					<a className="right brand-logo">Logo</a>
+					<ul className="left">
+						<li>
+							<a>Login With Google</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		);
 	}
 }
 
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,16 +1,16 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
 	app.use(
-		"/auth/google",
+		'/auth/google',
 		createProxyMiddleware({
-			target: "http://localhost:5000",
+			target: 'http://localhost:5000',
 			changeOrigin: true
 		})
 	);
 	app.use(
-		"/api/current_user",
+		'/api/*',
 		createProxyMiddleware({
-			target: "http://localhost:5000",
+			target: 'http://localhost:5000',
 			changeOrigin: true
 		})
 	);
