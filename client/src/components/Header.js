95ebe2198005213d88356ac517bb9b759d36009d
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-04-04 18:26:00 -0400

    client: adding header content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<li>
+						<a href="/auth/google">Login With Google</a>
+					</li>
+				);
+			default:
+				return (
+					<li>
+						<a>Logout</a>
+					</li>
+				);
+		}
+	}
+
 	render() {
 		return (
 			<nav>
 				<div className="nav-wrapper light-blue darken-4">
 					<a className="right brand-logo">Logo</a>
-					<ul className="left">
-						<li>
-							<a>Login With Google</a>
-						</li>
-					</ul>
+					<ul className="left">{this.renderContent()}</ul>
 				</div>
 			</nav>
 		);
 	}
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
