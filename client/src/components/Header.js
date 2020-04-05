3e43e3a1662a9fc9b2599f2a55f832fc25f0c1ea
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-04-05 07:13:03 -0400

    server: stripe configuration

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -26,7 +26,10 @@ class Header extends Component {
 		return (
 			<nav>
 				<div className="nav-wrapper light-blue darken-4">
-					<Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">
+					<Link
+						to={this.props.auth ? '/surveys' : '/'}
+						className="left brand-logo"
+					>
 						Malify
 					</Link>
 					<ul className="right">{this.renderContent()}</ul>
