746d911f0687d0042b7eeda4f6766a1be202327b
Mouad Lousimi <mouad.lousimi@etud.iga.ac.ma>
2020-04-27 03:19:31 -0400

    add docker-compose

---
+++ frontend/build/precache-manifest.1d33b23bed977cfab3caddc148da89c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578562f1b1a869b2fd1d2e7ebcadcfd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6dae907154e5ea1d5a7c",
+    "url": "/static/js/2.65a1212e.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.65a1212e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7361fdb82a83f4edd536",
+    "url": "/static/js/main.2d94e89a.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
